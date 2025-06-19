144205c002ead2173d2597fe6cd59e0974cd115d
Siddhant Gujrathi <162826971+SiddhantGujrathi@users.noreply.github.com>
2025-06-20 00:05:31 +0530

    Add Dockerfile for containerization

---
+++ src/main/java/com/siddhantgujrathi/EnterpriseDataProcessor/EnterpriseDataProcessorApplication.java
@@ -0,0 +1,13 @@
+package com.siddhantgujrathi.EnterpriseDataProcessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnterpriseDataProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnterpriseDataProcessorApplication.class, args);
+	}
+
+}
